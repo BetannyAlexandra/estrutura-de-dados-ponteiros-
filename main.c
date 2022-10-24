5346005c34131122b9d72e91fd0006692ec05a67
Bétanny <ainateb1972@gmail.com>
2022-10-24 16:15:41 -0300

    lista monitoria com menu

--- main.c
+++ main.c
@@ -37,45 +37,41 @@ void inserirfim(celula **head, int valor)
     {
         while (aux->prox != NULL)
         {
-           
-        aux = aux->prox; }
 
-            
-        
-        aux->prox=nova;
-        nova->ant=aux;
+            aux = aux->prox;
+        }
+
+        aux->prox = nova;
+        nova->ant = aux;
     }
 }
-
 
 void inserirmeio(celula **head, int valor, int ref)
 {
     celula *aux = *head;
     celula *nova = (celula *)malloc(sizeof(celula));
     nova->conteudo = valor;
-    if(*head==NULL){
-        nova->prox=NULL;
-        nova->ant=NULL;
-        *head=nova;
-    }else
-   { while (aux->conteudo != ref && aux->prox!=NULL)
-    { 
-     aux=aux->prox;
-
+    if (*head == NULL)
+    {
+        nova->prox = NULL;
+        nova->ant = NULL;
+        *head = nova;
     }
-    nova->prox=aux->prox;
-    if(aux->prox!=NULL){
-aux->prox->ant=nova;
-
-    }
-    nova->ant=aux;
-    aux->prox = nova;
-}
-   
-       
-
+    else
+    {
+        while (aux->conteudo != ref && aux->prox != NULL)
+        {
+            aux = aux->prox;
         }
-
+        nova->prox = aux->prox;
+        if (aux->prox != NULL)
+        {
+            aux->prox->ant = nova;
+        }
+        nova->ant = aux;
+        aux->prox = nova;
+    }
+}
 
 void concatenar(celula *head, celula *head2)
 {
@@ -100,16 +96,59 @@ void imprimir(celula *aux)
 
 int main()
 {
-celula *c=NULL;
-    inseririnicio(&c,5);
-    inseririnicio(&c,6);
-     inseririnicio(&c,7);
-     /*inserirfim(&c,10);
-     inserirfim(&c,11);*/
-     inserirmeio(&c,10,6);
-    
-     imprimir(c);
-     
+    celula *c = NULL;
+    int cel=7, valor,ref;
+
+    while (cel != 0){ 
+    printf("Preencha a lista\n");
+    printf("1-Inserir no inicio\n");
+    printf("2-Inserir no fim\n");
+    printf("3-Inserir no meio\n");
+    printf("4-concatenar\n");
+    printf("4-Imprimir\n");
+    printf("0-sair\n");
+    scanf("%d", &cel);
+
+
+        switch (cel)
+        {
+        case 1:
+            printf("informe o valor que deseja inserir\n");
+            scanf("%d", &valor);
+            inseririnicio(&c, valor);
+            break;
+
+        case 2:
+            printf("após que valor deseja inserir?\n");
+            scanf("%d", &ref);
+            printf("informe o valor que deseja inserir\n");
+            scanf("%d", &valor);
+            inserirmeio(&c, valor,ref);
+            break;
+        case 3:
+            printf("informe o valor que deseja inserir\n");
+            scanf("%d", &valor);
+            inserirfim(&c, valor);
+
+        case 4:
+        imprimir(c);
+        break;
+        case 0:
+
+        break;
+        default:
+            break;
+        }
+    }
+
+    /*inseririnicio(&c, 6);
+    inseririnicio(&c, 7);
+    /*inserirfim(&c,10);
+    inserirfim(&c,11);
+    inserirmeio(&c, 10, 6);
+
+    imprimir(c);*/
 
     return 0;
+    
 }
